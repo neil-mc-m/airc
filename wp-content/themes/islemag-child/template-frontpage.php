69f1659b1a33c910951bf53cfa464ff13fe5bc5a
studentneil <neilo2000@gmail.com>
2016-11-06 23:32:00 +0000

    added committee-members page, small work to front-page

--- wp-content/themes/islemag-child/template-frontpage.php
+++ wp-content/themes/islemag-child/template-frontpage.php
@@ -4,10 +4,10 @@
  */
 get_header(); ?>
 <div class="jumbotron">
-<div class="container-fluid"></div>
+    <div class="container-fluid"></div>
 </div>
     <div id="primary" class="content-area">
-        <div class="islemag-content-left col-md-12">
+        <div class="islemag-content-left">
             <main id="main" class="site-main" role="main">
                 <div class="container intro-block">
                     <div class="row">
@@ -18,62 +18,62 @@ get_header(); ?>
                 </div>
                 <div class="container">
                     <div class="row">
-               <div class="col-md-4 blog">
-                   <h1 class="text-center">FOLLOW OUR BLOG</h1>
-                   <?php
-                   $latest_blog_posts = new WP_Query( array( 'posts_per_page' => 1 ) );
+                       <div class="col-md-4 blog">
+                           <h1 class="text-center">FOLLOW OUR BLOG</h1>
+                           <?php
+                           $latest_blog_posts = new WP_Query( array( 'posts_per_page' => 1 ) );
 
-                   if ( $latest_blog_posts->have_posts() ) : while ( $latest_blog_posts->have_posts() ) : $latest_blog_posts->the_post();
-                   // Loop output goes here
-                       get_template_part( 'template-parts/content', get_post_format() );
-                    endwhile;
-                   endif;
-                   ?>
+                           if ( $latest_blog_posts->have_posts() ) : while ( $latest_blog_posts->have_posts() ) : $latest_blog_posts->the_post();
+                           // Loop output goes here
+                               get_template_part( 'template-parts/content', get_post_format() );
+                            endwhile;
+                           endif;
+                           ?>
 
 
-               </div>
-                <div class="col-md-4 events">
-                    <h1 class="text-center">UPCOMING EVENTS</h1>
-                    <?php
-                    $args = array(  'post_type' => 'events',
-                        'posts_per_page' => 1
-                    );
-                    $loop = new WP_Query( $args );
-                    while ( $loop->have_posts() ) : $loop->the_post();
-                        get_template_part('template-parts/content', get_post_format());
+                       </div>
+                        <div class="col-md-4 events">
+                            <h1 class="text-center">UPCOMING EVENTS</h1>
+                            <?php
+                            $args = array(  'post_type' => 'events',
+                                'posts_per_page' => 1
+                            );
+                            $loop = new WP_Query( $args );
+                            while ( $loop->have_posts() ) : $loop->the_post();
+                                get_template_part('template-parts/content', get_post_format());
 
-                    endwhile;// End of the loop.
-                    ?>
-                </div>
-                <div class="col-md-4 downloads">
-                    <h1 class="text-center">DOWNLOADS</h1>
-                    <?php
-                    $args = array(  'post_type' => 'downloads',
-                        'posts_per_page' => 1
-                    );
-                    $loop = new WP_Query( $args );
-                    while ( $loop->have_posts() ) : $loop->the_post();
-                        get_template_part('template-parts/content', get_post_format());
+                            endwhile;// End of the loop.
+                            ?>
+                        </div>
+                        <div class="col-md-4 downloads">
+                            <h1 class="text-center">DOWNLOADS</h1>
+                            <?php
+                            $args = array(  'post_type' => 'downloads',
+                                'posts_per_page' => 1
+                            );
+                            $loop = new WP_Query( $args );
+                            while ( $loop->have_posts() ) : $loop->the_post();
+                                get_template_part('template-parts/content', get_post_format());
 
-                    endwhile;// End of the loop.
-                    ?>
-
-                </div>
+                            endwhile;// End of the loop.
+                            ?>
                         </div>
                     </div>
-                        <div class="container">
-                            <div class="row">
-                <div class="col-md-4">
-                    <h1 class="text-center">
-                        KNOW YOUR RIGHTS
-                    </h1>
-                    </div>
-                                </div>
-                            </div>
                 </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <h1 class="text-center">KNOW YOUR RIGHTS</h1>
+
+
+                            <p class="pull-right">Know your rights and entitlements when caring for your child.</p>
+                            <a href="<?php echo get_page_link( get_page_by_title( 'rights and entitlements' )->ID ); ?>">Parents Rights</a>
+                        </div>
+                    </div>
+                </div>
+
             </main><!-- #main -->
         </div><!-- #primary -->
-    </div><!-- #primary -->
-    </div>
-</div>
+   </div><!-- #primary -->
+
 <?php get_footer(); ?>
---
+++ wp-content/themes/islemag-child/template-parts/content-committee.php
@@ -0,0 +1,55 @@
+<?php
+/*
+* Template part for displaying the page content in page.php
+*/
+?>
+<div class="col-md-3">
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <header class="entry-header text-center">
+        <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+    </header><!-- .entry-header -->
+
+    <div class="entry-content">
+        <figure class="text-center">
+            <a href="<?php the_permalink(); ?>">
+                <?php
+                $islemag_thumbnail_id = get_post_thumbnail_id();
+                if($islemag_thumbnail_id){
+                    $islemag_thumb_meta = wp_get_attachment_metadata($islemag_thumbnail_id);
+                    if($islemag_thumb_meta['width'] > 250 && $islemag_thumb_meta['height'] > 250 ) {
+                        if( $islemag_thumb_meta['width'] / $islemag_thumb_meta['height'] > 1.5 ){
+                            the_post_thumbnail('islemag_blog_post');
+                        } else {
+                            the_post_thumbnail('islemag_blog_post_no_crop');
+                        }
+                    }
+                } else {
+                    echo '<img src="' . get_template_directory_uri() . '/img/blogpost-placeholder.jpg" />';
+                } ?>
+            </a>
+        </figure>
+        <?php the_content(); ?>
+        <?php
+        wp_link_pages( array(
+            'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'islemag' ),
+            'after'  => '</div>',
+        ) );
+        ?>
+    </div><!-- .entry-content -->
+
+
+    <?php
+    edit_post_link(
+        sprintf(
+        /* translators: %s: Name of current post */
+            esc_html__( 'Edit %s', 'islemag' ),
+            the_title( '<span class="screen-reader-text">"', '"</span>', false )
+        ),
+        '<footer class="entry-footer"><span class="edit-link">',
+        '</span></footer>'
+    );
+    ?>
+
+</article><!-- #post-## -->
+</div><!--col-md-4-->
